7edefa8d6b50fc19d9d63fcd899d80b0187a4c14
pewdiecal <26195377+Pewdiecal@users.noreply.github.com>
2020-06-26 00:11:34 +0800

    Added FAB animation

--- app/src/main/java/com/caltech/autoattend/MainActivity.java
+++ app/src/main/java/com/caltech/autoattend/MainActivity.java
@@ -2,12 +2,17 @@ package com.caltech.autoattend;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     BottomAppBar bottomAppBar;
     ViewPager2 viewPager2;
     MainFragmentAdapter mainFragmentAdapter;
+    FloatingActionButton mainAddFab;
+    FloatingActionButton qrCodeFab;
+    FloatingActionButton linkFab;
+    ConstraintLayout qrCodeLayout;
+    ConstraintLayout linkLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +36,17 @@ public class MainActivity extends AppCompatActivity {
 
         bottomAppBar = findViewById(R.id.bottomAppBar4);
         viewPager2 = findViewById(R.id.host_viewPager2);
+        mainAddFab = findViewById(R.id.add_fab);
+        qrCodeFab = findViewById(R.id.qr_code_fab);
+        linkFab = findViewById(R.id.link_fab);
+        qrCodeLayout = findViewById(R.id.qrcode_layout);
+        linkLayout = findViewById(R.id.link_layout);
         mainFragmentAdapter = new MainFragmentAdapter(this);
         viewPager2.setAdapter(mainFragmentAdapter);
+        Animation fabShow = AnimationUtils.loadAnimation(this, R.anim.fab_show);
+        Animation fabHide = AnimationUtils.loadAnimation(this, R.anim.fab_hide);
+        Animation fabFadeIn = AnimationUtils.loadAnimation(this, R.anim.fab_fade_in);
+        Animation fabFadeOut = AnimationUtils.loadAnimation(this, R.anim.fab_fade_out);
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -45,6 +64,34 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        mainAddFab.setOnClickListener(v -> {
+            bottomAppBar.performShow();
+            if (qrCodeLayout.getVisibility() == View.VISIBLE && linkLayout.getVisibility() == View.VISIBLE) {
+                qrCodeLayout.setVisibility(View.GONE);
+                linkLayout.setVisibility(View.GONE);
+                mainAddFab.startAnimation(fabHide);
+                qrCodeLayout.startAnimation(fabFadeOut);
+                linkLayout.startAnimation(fabFadeOut);
+            } else {
+                qrCodeLayout.setVisibility(View.VISIBLE);
+                linkLayout.setVisibility(View.VISIBLE);
+                mainAddFab.startAnimation(fabShow);
+                qrCodeLayout.startAnimation(fabFadeIn);
+                linkLayout.startAnimation(fabFadeIn);
+            }
+        });
+
+        linkFab.setOnClickListener(v -> {
+            qrCodeLayout.setVisibility(View.GONE);
+            linkLayout.setVisibility(View.GONE);
+            mainAddFab.startAnimation(fabHide);
+            qrCodeLayout.startAnimation(fabFadeOut);
+            linkLayout.startAnimation(fabFadeOut);
+            Intent mainIntent = new Intent(this, AddSubject.class);
+            startActivity(mainIntent);
+        });
+
 
     }
 
